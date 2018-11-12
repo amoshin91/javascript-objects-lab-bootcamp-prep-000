637fa69803fb447f31487fb2cb12659494867b6e
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-12 21:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   recipes['prop'] = '1'
   return recipes;
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes['prop2'] = '2'
+  return recipes;
+}
